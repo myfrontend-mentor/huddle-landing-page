d3c80b9db92e48c78486a785080a4d5ec9cd3af3
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-04-14 12:16:13 +0300

    fix: update the base url

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     // vueDevTools(),
   ],
-  base: './',
+  base: '/huddle-landing-page/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
